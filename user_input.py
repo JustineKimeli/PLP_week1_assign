48e25e7c697e59bc4e66e53fb906c7bb927aaa88
JustineKimeli <92584953+JustineKimeli@users.noreply.github.com>
2024-02-25 15:09:43 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,6 @@
+Name=input("Enter you name:")
+Age= input("Enter your age:")
+Location=input("Enter your current location:")
+
+# display the output of inputs
+print(f"Hello my name is{Name},I am {Age} years old and I come from {Location}")
